03114818e41910e28b09b2e70edb6c3ea29c17d4
Sergey <sk@atomiqconsulting.com>
2020-03-04 20:30:26 +0200

    react-chat

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+  <App />
+</Router>
+ , document.getElementById('root'));
